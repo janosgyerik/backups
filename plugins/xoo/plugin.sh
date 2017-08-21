743b8cb3e3184c53b521a2a91496070f82cf163d
Janos Gyerik <janos@kronos>
2017-08-21 21:47:35 +0200

    Make it easier to reuse xoo plugin as template

--- plugins/xoo/plugin.sh
+++ plugins/xoo/plugin.sh
@@ -1,12 +1,15 @@
 validate_args() {
-    shift
+    local name=$1; shift
     test $# = 0 || fatal "excess arguments: $@"
 }
 
 run() {
-    # params: plugin name workdir
     # output: path to backup file
+    local plugin=$1; shift
+    local name=$1; shift
+    local workdir=$1; shift
+
     local backupfile=xoo.zip
-    touch "$3/$backupfile"
+    touch "$workdir/$backupfile"
     echo $backupfile
 }
