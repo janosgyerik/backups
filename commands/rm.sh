fdc9465b2b14bee0c21d4725fe93c4055c828189
Janos Gyerik <janos@kronos>
2017-08-21 21:06:30 +0200

    Wrap the main operations of rm command in a function

--- commands/rm.sh
+++ commands/rm.sh
@@ -1,14 +1,18 @@
 # remove a backup configuration
 
-plugin=$1; shift
-validate_plugin $plugin
+main() {
+    local plugin=$1; shift
+    validate_plugin $plugin
 
-name=$1; shift
-validate_name $plugin "$name"
-validate_config_exists $plugin $name
+    local name=$1; shift
+    validate_name $plugin "$name"
+    validate_config_exists $plugin $name
 
-validate_no_more_args "$@"
+    validate_no_more_args "$@"
 
-rm_config $plugin $name
-#TODO
-#rm_crontab $plugin $name
+    rm_config $plugin $name
+    #TODO
+    #rm_crontab $plugin $name
+}
+
+main "$@"
