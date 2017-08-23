34d7600d4e366d2fbeec2c9947c1ccf99ead2f01
Janos Gyerik <janos@kronos>
2017-08-23 21:20:49 +0200

    Create work dir for each test

--- run-tests.sh
+++ run-tests.sh
@@ -11,21 +11,22 @@ cleanup() {
 }
 
 trap 'cleanup; exit' 0 1 2 3 15
-work=$(mktemp -d)
-
-export WORK=$work/work
-export CONF=$work/conf
-export BACKUPS=$work/backups
-mkdir -p "$WORK" "$CONF" "$BACKUPS"
 
 export MAIN=$PWD/backups.sh
-export CRONTAB=cat
 #crontab -l 2>/dev/null | sed -e "\?^$cron_unique_label\$?,/^\$/ d" | crontab -
 
 test $# -gt 0 || set -- tests/* plugins/*/tests.sh
 
 for testscript; do
     test -f "$testscript" || continue
+
+    work=$(mktemp -d)
+    export WORK=$work/work
+    export CONF=$work/conf
+    export BACKUPS=$work/backups
+    mkdir -p "$WORK" "$CONF" "$BACKUPS"
+
     msg running tests: $testscript ...
     $testscript
+    cleanup
 done
