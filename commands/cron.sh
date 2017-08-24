115151d2960e28b8db0dc3e6361484838f4bcc92
Janos Gyerik <janos@kronos>
2017-08-24 23:19:13 +0200

    Add cron command

---
+++ commands/cron.sh
@@ -0,0 +1,51 @@
+# run configurations of specified period
+
+get_plugins() {
+    ls -d plugins/*/ | cut -f2 -d/
+}
+
+get_configs() {
+    local plugin=$1; shift
+    local path=$CONF/$plugin
+
+    if test -d "$path"; then
+        find "$path" -type f | sed -e 's?.*/??' -e 's/\.sh$//'
+    fi
+}
+
+validate_periodname() {
+    local name=$1
+    case "$name" in
+        daily|weekly|monthly|hourly) ;;
+        *) fatal "invalid period name: '$name'"
+    esac
+}
+
+get_period() {
+    local shortname
+    case "$1" in
+        daily) shortname=d ;;
+        weekly) shortname=w ;;
+        monthly) shortname=m ;;
+        hourly) shortname=h ;;
+    esac
+    echo $shortname
+}
+
+cmd() {
+    local periodname=$1; shift
+    validate_periodname "$periodname"
+
+    validate_no_more_args "$@"
+
+    period=$(get_period $periodname)
+
+    for plugin in $(get_plugins); do
+        for config in $(get_configs $plugin); do
+            load_config $plugin $config
+            if [[ $periods == *$period* ]]; then
+                $MAIN run $plugin $config
+            fi
+        done
+    done
+}
---
+++ tests/52-cron.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+. ./include/functions.sh
+. ./include/test-functions.sh
+
+fail cron x_nonexistent_period
+
+ok add xoo example1 d
+ok add xoo example2 dw
+ok add xoo example3 wm
+
+fail cron daily excess_arg
+ok cron daily
+
+backups_exist xoo example1 d
+backups_exist xoo example2 d
+
+fail cron weekly excess_arg
+ok cron weekly
+
+backups_exist xoo example2 w
+backups_exist xoo example3 w
+
+fail cron monthly excess_arg
+ok cron monthly
+
+backups_exist xoo example3 m
+
+summary
