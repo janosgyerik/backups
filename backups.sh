11a115349a14faa15353b84bf82ae033a1fb0cf6
Janos Gyerik <janos@kronos>
2017-09-02 18:19:43 +0200

    Add command descriptions in help

--- backups.sh
+++ backups.sh
@@ -27,13 +27,13 @@ usage() {
     echo
     echo Commands:
     echo
-    echo "  add PLUGIN NAME PERIODS [ARGS...]"
-    echo "  config [PLUGIN [NAME]]"
-    echo "  cron [daily|weekly|month|hourly]"
-    echo "  list [PLUGIN [NAME]]"
-    echo "  remove PLUGIN NAME"
-    echo "  run PLUGIN NAME [PERIODS]"
-    echo "  plugins"
+    echo "  plugins                               List available plugins"
+    echo "  add PLUGIN NAME PERIODS [ARGS...]     Add backup configuration"
+    echo "  config [PLUGIN [NAME]]                List backup configurations"
+    echo "  list [PLUGIN [NAME]]                  List backup files"
+    echo "  remove PLUGIN NAME                    Remove backup configuration"
+    echo "  run PLUGIN NAME [PERIODS]             Execute backup configurations"
+    echo "  cron [daily|weekly|month|hourly]      Execute backup configurations for period"
     echo
     exit 1
 }
